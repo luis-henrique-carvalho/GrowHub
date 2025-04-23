0e3597510f6defd0ca5f2f5cb66859c50dee55a2
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-04-23 01:49:37 -0300

    Remove unused models, operations, and schemas for categories and transactions

--- app/operations/transactions/show.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Transactions
-  class Show < ApplicationOperation
-    def call(transaction)
-      success transaction
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_03_04_181100) do
+  # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
